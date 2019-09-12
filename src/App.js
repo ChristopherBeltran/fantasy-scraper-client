e485bb54e5b41adde64afb541ccbb437bf727894
Christopher Beltran <christopherbeltran@Christophers-MacBook-Pro.local>
2019-09-12 14:00:45 -0600

    Removing Auth0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-wrapper";
 
 function App() {
-  const { loading } = useAuth0();
 
   if (loading) {
     return (
