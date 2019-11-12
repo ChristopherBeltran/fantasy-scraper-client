fb5b8bec09e3a1ccf3fb03047d448ebc3a26299c
github username <github email address>
2019-11-11 19:32:51 -0700

    Reinitializing node_modules and updating yahooLogin to use the signInWithRedirect method

--- src/actions/Login.js
+++ src/actions/Login.js
@@ -5,28 +5,72 @@
 //const firebaseui = require('firebaseui');
 
 //const provider = new firebase.auth.OAuthProvider('yahoo.com');
-import * as firebase from "firebase/app";
+import * as firebase from 'firebase';
+
+var provider = new firebase.auth.OAuthProvider('yahoo.com');
 
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBouIAxKO8yEqgeYvuop_xuzv2u7Bxg-6E",
+  authDomain: "fantasy-news-ddeeb.firebaseapp.com",
+  databaseURL: "https://fantasy-news-ddeeb.firebaseio.com",
+  projectId: "fantasy-news-ddeeb",
+  storageBucket: "fantasy-news-ddeeb.appspot.com",
+  messagingSenderId: "620542976138",
+  appId: "1:620542976138:web:d4e9350aa95cce41c98d73"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+//export const yahooLogin = (history) => {
+//    return dispatch => {
+//        provider.addScope('fspt-r');
+//        firebase.auth().signInWithPopup(provider)
+//  .then(function(result) {
+//    // User is signed in.
+//    // IdP data available in result.additionalUserInfo.profile.
+//    // Yahoo OAuth access token can be retrieved by calling:
+//    // result.credential.accessToken
+//    // Yahoo OAuth ID token can be retrieved by calling:
+//    // result.credential.idToken
+//    localStorage.setItem('sessionID', result.credential.idToken)
+//    history.push('/dashboard')
+//  })
+//  .catch(function(error) {
+//    console.log(error)
+//  });
+//    }    
+//}
+
+export const postLogin = () => {
+  debugger;
+  return dispatch => {
+    firebase.auth().getRedirectResult()
+    .then(function(result) {
+      console.log(result)
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
+}
+}
 
 export const yahooLogin = (history) => {
     return dispatch => {
-        const provider = new firebase.auth.OAuthProvider('yahoo.com');
         provider.addScope('fspt-r');
-        firebase.auth().signInWithPopup(provider)
-  .then(function(result) {
-    // User is signed in.
-    // IdP data available in result.additionalUserInfo.profile.
-    // Yahoo OAuth access token can be retrieved by calling:
-    // result.credential.accessToken
-    // Yahoo OAuth ID token can be retrieved by calling:
-    // result.credential.idToken
-    localStorage.setItem('sessionID', result.credential.idToken)
-    history.push('/dashboard')
-  })
-  .catch(function(error) {
-    // Handle error.
-  });
-    }    
+        firebase.auth().signInWithRedirect(provider)
+        .then(r => r.json())
+        .then(response => {
+          if (response.error) {
+            alert(response.error)
+          } else {
+            history.push('/dashboard')
+          }
+          // Handle error.
+        })
+    } 
 }
+
+//TO-DO: 
